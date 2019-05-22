6351e4e86d3782d11d1bb63d6f401c95f9c2804b
KeeylaSilva <keylasilvarosa@gmail.com>
2019-05-22 15:58:53 -0300

    Edição do JS, criação do IF e ELSE do M Maravilhosas para colocação de IMG

--- 7 Semana/Consumo de API/script.js
+++ 7 Semana/Consumo de API/script.js
@@ -17,22 +17,31 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
             // box.setAttribute('class', 'box')
             // woman.appendChild(box)
 
-            const link = document.createElement('div')
-            link.setAttribute('class', 'maravilhosas__perfil')
+            const link = document.createElement('maravilhosas__perfil')
+            link.setAttribute('class', 'a')
             woman.appendChild(link)
 
-            // const img = document.createElement('a')
-            // img.setAttribute('src', data.image)
-            // link.appendChild(img)
-           
+
+
             const title = document.createElement('h3')
             title.innerHTML = data.title
             link.appendChild(title)
-            
+
             const descricao = document.createElement('p')
             descricao.innerHTML = data.description
             link.appendChild(descricao)
-    
+
+            const a = document.createElement('a')
+            a.setAttribute('src', 'Foto da personalidade')
+            link.appendChild(a)
+
+            const img = document.createElement('img')
+            if (data.metadata && data.metadata.image.url) {
+                img.setAttribute('src', data.metadata.image.url);
+            } else {
+                // img.setAttribute('src', '.Consumo de API\img\img-mulher.png')
+            }
+            link.appendChild(img)
 
             // CRIAR IF PARA IMAGENS
         })
