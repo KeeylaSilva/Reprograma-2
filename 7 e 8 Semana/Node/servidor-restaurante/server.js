4b8a5926efe8b71996887323ee0ff16abf9734b7
KeeylaSilva <keylasilvarosa@gmail.com>
2019-05-28 22:15:50 -0300

    Primeiro servidor com nodemon e duas rotas

--- 7 e 8 Semana/Node/servidor-restaurante/server.js
+++ 7 e 8 Semana/Node/servidor-restaurante/server.js
@@ -1,1 +1,21 @@
 const http = require('http')
+//Subindo servidor
+const servidor = http.createServer(function (request, response) {
+    if (request.url === '/') {
+        response.end('Hello World')
+        //declarando ROTA ('/comidas')
+    } else if (request.url === '/comidas') {
+        if (request.method === 'GET') {
+            //Adicionando leitor para HTML e UTF8
+            response.writeHead(200, {"Content-type": "text/html;charset=utf-8"})
+            response.end("<h1>Servidosão das PlayBA</h1>")
+        }else if (response.method === 'POST'){
+            response.writeHead(201, { "Content-type": "text/html;charset=utf-8" })  
+            response.end("<h1>VEM CURTIRRRRRRRR!!!!!</h1>")
+        }
+
+       
+    }
+})
+
+servidor.listen(3000)
